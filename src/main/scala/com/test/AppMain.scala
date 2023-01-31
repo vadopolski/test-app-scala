d00ad210bfcef9be10525b1070a4354c3ff5afb9
Vadim <vadopolski@gmail.com>
2023-01-31 22:02:19 +0100

    Added config and libs

--- src/main/scala/com/test/AppMain.scala
+++ src/main/scala/com/test/AppMain.scala
@@ -5,92 +5,130 @@ import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
 
+import java.nio.file.{Files, Paths}
 import scala.io.{BufferedSource, Source}
-import scala.util.{Try, Using}
+import scala.util.{Failure, Success, Try, Using}
 
 object AppMain extends App {
-    println(s"Source path ${args(0)}")
-    println(s"Target path ${args(1)}")
+    case class Country(
+                        name: Name,
+                        capital: List[String],
+                        area: Double, // required
+                        region: Option[String]) // optional
+
+    case class Name(common: String)
+
+    case class CountryDto(
+                           name: String,
+                           capital: String,
+                           area: Double
+                         )
+
+    object CountryDto {
+        // TODO:  added chimney
+        def createDto(country: Country): CountryDto =
+            CountryDto(
+                name = country.name.common,
+                capital = country.capital.head,
+                area = country.area
+            )
+    }
+
+    implicit val inputCodec: JsonValueCodec[List[Country]] = JsonCodecMaker.make
+    implicit val outputCodec: JsonValueCodec[List[CountryDto]] = JsonCodecMaker.make
+
+    // TODO: Move to args
+    val value = "src/main/resources/countries.json"
+
+    val triedString: Try[List[Country]] = Using(Source.fromFile(value)) { source =>
+      readFromString[List[Country]](source.mkString)
+    }
+
+    triedString match {
+        case Success(countries) =>
+            println(s"Source json was successfully red and validated, size is = ${countries.size}")
+
+            val dtoes: List[CountryDto] = countries
+              .filter(x => x.region.contains("Africa"))
+              .sortBy(x => -x.area)
+              .take(10)
+              .map(x => CountryDto.createDto(x))
+
+            println(s"Data successfully processed and converted to target size = ${dtoes.size}")
+
+            // TODO: Move to args and to scopt
+            val filename: String = "output.json"
 
 
-    sealed trait Foo
-    case class Bar(xs: Vector[String]) extends Foo
-    case class Qux(i: Int, d: Option[Double]) extends Foo
+            Try(Files.write(Paths.get(filename), writeToArray(dtoes))) match {
+                // TODO: Add exception
+                case Failure(exception) => ???
+                case Success(path) => println(s"Processed file successfully written to $path")
+            }
 
-    val foo: Foo = Qux(13, Some(14.0))
-    println(foo)
 
-    Using(Source.fromFile("src/main/resources/countries.json")) {byte =>
-        val source = byte.mkString
-        println(source)
+        case Failure(exception) => exception match {
+            case ex: com.github.plokhotnyuk.jsoniter_scala.core.JsonReaderException => println(s"ERROR ${ex.getMessage}")
+            case _ => exception.printStackTrace()
+        }
 
-        val value = decode[List[Foo]](source)
+    }
 
-        value match {
-            case Right(value) => value
-            case Left(error) => println(error)
 
+
+/*
+    triedString match {
+        case Success(value) => {
+            val countries = {
+                readFromArray[List[Country]](value.getBytes)
+            }
+            val result = countries
+              .filter(_.region == "Africa")
+              .map(c => CountryDto.createDto(c))
+              .sortBy(-_.area).take(10)
+            val resultJson = writeToArray(result)
+
+            val filename: String = "output.json"
+            val path = Paths.get(filename)
+            try {
+                Files.write(path, resultJson)
+            } catch {
+                case e: Exception => {
+                    println(e)
+                }
+            }
+        }
+        case Failure(exception) => println(s"Could you pls check the source file")
+    }
+
+    triedString
+      .flatMap(x => ??? )
+      .recover{case ex: Exception => println(s"Could you pls check the source file") }
+
+
+    triedString.foreach { s =>
+        val countries = readFromArray[List[Country]](s.getBytes)
+        val result = countries
+          .filter(_.region == "Africa")
+          .map(c => CountryDto.createDto(c))
+          .sortBy(-_.area).take(10)
+        val resultJson = writeToArray(result)
+
+        val filename: String = "output.json"
+        val path = Paths.get(filename)
+        try {
+            Files.write(path, resultJson)
+        } catch {
+            case e: Exception => {
+                println(e)
+            }
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    val i: Int = Option(5) match {
-        case Some(value) => value + 5
-        case None => 0
-    }
-
-
-
-/*
-    T => Monad[T]
-    List[Int] => Int => List[Int]
-    Option[Int] => Option[Int]
-
-    val maybeInt: Option[Int] = Option(5)
 */
 
-    val l:  Int => Option[Int] = t => Option(t) // unit
-    val l2: Option[Int] => Int = (opt: Option[Int]) => opt.get //
-
-/*
-    val map: Option[Int] = Option(5).map(x => x + 5)
-    val map2: Option[Option[Int]] = Option(5).map(x => Option(x + 5))
-    val flatMap: Option[Int] = Option(5).flatMap(x => Option(x + 5))
-*/
-
-    val map = List(5).map(x => x + 5)
-    val map2 = List(5).map(x => List(x + 5))
-    val flatMap = List(5).flatMap(x => List(x + 5))
-
-    /*
-    *
-    * */
-
-/*
-    val json: String = foo.asJson.noSpaces
-    println(json)
-//  String (json) => object
 
 
-    val ex = """{"Qux":{"i":13,"d":14.0}}"""
-    val ex2 = """{"Qux":{"i":13,"d":14.0}}"""
 
-    val decodedFoo: Either[Error, Foo] = decode[Foo](json)
-    println(decodedFoo.right)
-*/
 
 
 
